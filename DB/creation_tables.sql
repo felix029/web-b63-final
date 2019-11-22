cabd7cc80003a57aea80887ec5931d8f84d767c8
Felix-Olivier Landry <felixo2997@gmail.com>
2019-11-22 18:53:24 -0500

    added visibility to sql table

--- DB/creation_tables.sql
+++ DB/creation_tables.sql
@@ -2,12 +2,13 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT,
-	username VARCHAR(16),
-	pwd VARCHAR(64),
+	username VARCHAR(16) NOT NULL,
+	pwd VARCHAR(64) NOT NULL,
+	visibility INT NOT NULL,
 	
 	PRIMARY KEY pk_users(id),
 	CONSTRAINT uc_users_username UNIQUE (username)
 
 ) engine = innoDB character set utf8 collate utf8_general_ci;
 
-INSERT INTO users(username, pwd) VALUES ('dkadmin', '3c4506cbcf214e37faea109715d14d9959666b87defe50061b85e2daaaec7616');
+INSERT INTO users(username, pwd, visibility) VALUES ('dkadmin', '3c4506cbcf214e37faea109715d14d9959666b87defe50061b85e2daaaec7616', 3);
