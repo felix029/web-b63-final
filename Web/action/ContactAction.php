8abc163a95d855b88195a13a5e9e1ea8a5be2071
Felix-Olivier Landry <felixo2997@gmail.com>
2019-11-30 21:53:54 -0500

    email done

--- Web/action/ContactAction.php
+++ Web/action/ContactAction.php
@@ -13,10 +13,12 @@
 
 			if(isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['courriel']) && isset($_POST['message'])){
 				$subject = "Message from DKoncept.com";
-				$headers = "From: " . $_POST['prenom'] . " " . $_POST['nom'] . " <" . $_POST['courriel'] . ">\r\n";
+				$headers = "From: " . $_POST['prenom'] . " " . $_POST['nom'] . " <noreply@dkoncept.com>\r\n";
 				$headers .= "Reply-To: " . $_POST['courriel'] . "\r\n";
-				
-				mail("felixo2997@gmail.com", $subject, $_POST['message'], $headers);
+				$message = "Message from " . $_POST['prenom'] . " " . $_POST['nom'] . " (" . $_POST['courriel'] . ")\n\n";
+				$message .= $_POST['message'];
+
+				mail("felixo2997@gmail.com", $subject, $message, $headers);
 
 				unset($_POST['prenom']);
 				unset($_POST['nom']);
