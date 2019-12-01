47175ca10fa5d474e5294d0b94f74edca9199967
Felix-Olivier Landry <felixo2997@gmail.com>
2019-12-01 12:30:11 -0500

    quick css adjustment

--- Web/contact.php
+++ Web/contact.php
@@ -14,7 +14,7 @@
 		<div>
 			Situez en bord de mer en banlieu de la ville nord-côtière de Sept-Îles, DKoncept saura vous charmez, venez y faire un tour!
 		</div>
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2553.6128181553086!2d-66.2944628839271!3d50.20576777944228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4c91df2c32c32377%3A0x8cd256081bd21433!2s658%20Rue%20des%20Galets%2C%20Sept-%C3%8Eles%2C%20QC%20G4R%200B9!5e0!3m2!1sfr!2sca!4v1575156049525!5m2!1sfr!2sca" width="100%" height="70%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2553.6128181553086!2d-66.2944628839271!3d50.20576777944228!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4c91df2c32c32377%3A0x8cd256081bd21433!2s658%20Rue%20des%20Galets%2C%20Sept-%C3%8Eles%2C%20QC%20G4R%200B9!5e0!3m2!1sfr!2sca!4v1575156049525!5m2!1sfr!2sca" width="100%" height="80%" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 	</div>
 
 	<div id="contact">
