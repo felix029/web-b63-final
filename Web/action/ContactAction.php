514e88c6d9715a7074899b52982ea928df27437b
Felix-Olivier Landry <felixo2997@gmail.com>
2019-11-30 20:49:57 -0500

    test mail

--- Web/action/ContactAction.php
+++ Web/action/ContactAction.php
@@ -10,5 +10,19 @@
 		protected function executeAction() {
 			$_SESSION["editable"] = false;
 			$_SESSION["page"] = "contact.php";
+
+			if(isset($_POST['prenom']) && isset($_POST['nom']) && isset($_POST['courriel']) && isset($_POST['message'])){
+				$subject = "Message from DKoncept.com";
+				$headers = "From: " . $_POST['prenom'] . " " . $_POST['nom'] . " <" . $_POST['courriel'] . ">\r\n";
+				$headers .= "Reply-To: " . $_POST['courriel'] . "\r\n";
+				
+				mail("felixo2997@gmail.com", $subject, $_POST['message'], $headers);
+
+				unset($_POST['prenom']);
+				unset($_POST['nom']);
+				unset($_POST['courriel']);
+				unset($_POST['message']);
+			}
+
 		}
 	}
