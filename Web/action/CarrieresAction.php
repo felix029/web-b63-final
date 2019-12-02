fb91258c00b19446b2549258ffbf9e30fadc0844
Felix-Olivier Landry <felixo2997@gmail.com>
2019-12-02 15:19:24 -0500

    job application test

--- Web/action/CarrieresAction.php
+++ Web/action/CarrieresAction.php
@@ -120,21 +120,16 @@
 					$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" . 
 					"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n";
 
-					if(!empty($file) > 0){
-						if(is_file($file)){
-							$mesage .= "--{$mime_boundary}\n";
-							$fp = @fopen($file,"rb");
-							$data = @fread($fp, filesize($file));
-
-							@fclose($fp);
-							$data = chunk_split(base64_encode($data));
-							$message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" . 
-							"Content-Description: ".basename($file)."\n" .
-							"Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" . 
-							"Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-						}
-					}
-
+					$mesage .= "--{$mime_boundary}\n";
+					$fp = @fopen($file,"rb");
+					$data = @fread($fp, filesize($file))
+					@fclose($fp);
+					$data = chunk_split(base64_encode($data));
+					$message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" . 
+					"Content-Description: ".basename($file)."\n" .
+					"Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" . 
+					"Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+					
 					$message .= "--{$mime_boundary}--";
 					$returnpath = "-f" . $from;
 
